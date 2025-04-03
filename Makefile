de0d29a1dce020ba7ecbb69d52bb497d257060b3
michal <michalnu@post.bgu.ac.il>
2025-04-03 16:20:54 +0000

    Add Task1 & Task2

--- Makefile
+++ Makefile
@@ -132,6 +132,7 @@ UPROGS=\
 	$U/_grind\
 	$U/_wc\
 	$U/_zombie\
+	$U/_memsize_test #we added this
 
 fs.img: mkfs/mkfs README $(UPROGS)
 	mkfs/mkfs fs.img README $(UPROGS)
---
+++ user/memsize_test.c
@@ -0,0 +1,25 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int main() {
+    // (a) Print the initial memory size
+    printf("Initial memory size: %d bytes\n", memsize());
+
+    // (b) Allocate 20k more bytes of memory
+    char *allocated_memory = malloc(20 * 1024); // 20 KB
+    if (allocated_memory == 0) {
+        printf("Memory allocation failed\n");
+        exit(1);
+    }
+
+    // (c) Print memory size after allocation
+    printf("Memory size after allocation: %d bytes\n", memsize());
+
+    // (d) Free the allocated memory
+    free(allocated_memory);
+
+    // (e) Print memory size after releasing memory
+    printf("Memory size after release: %d bytes\n", memsize());
+
+    exit(0);
+}
